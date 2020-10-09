48a28821b34028936e81b701bb8f0757a82db58f
tsamantanis <philip.tsamantanis@icloud.com>
2020-10-09 15:10:18 -0700

    Initial setup

---
+++ src/components/Register/Register.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import logo from '../../logo.svg';
+import '../../App.css';
+
+class Register extends Component {
+    render() {
+        return (
+            <div className="Register">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Register;
