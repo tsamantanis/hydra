9f15cdfa176bab9d3c7bd6053328deffac2c4a9f
Starlight Romero <starlightromero@gmail.com>
2020-10-09 17:33:20 -0700

    Added SignIn component

---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import logo from '../../logo.svg'
+import '../../App.css'
+
+class SignIn extends Component {
+  render () {
+    return (
+      <div className='SignIn'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+        </header>
+      </div>
+    )
+  }
+}
+
+export default SignIN
