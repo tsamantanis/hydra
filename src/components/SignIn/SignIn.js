a90e6f214ca3d23cce7870faec764d47bacf10c1
Starlight Romero <starlightromero@gmail.com>
2020-10-09 19:59:10 -0700

    completed jsx for signin

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,14 +1,31 @@
 import React, {Component} from 'react'
-import logo from '../../logo.svg'
 import '../../App.css'
+import './SignIn.css'
+import happiness from '../../assets/happiness.svg'
 
 class SignIn extends Component {
   render () {
     return (
       <div className='SignIn'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-        </header>
+        <div className='left'>
+          <form>
+            <legend>
+              <h1>Sign In</h1>
+              <h6>Access your messages, tutorials, and more</h6>
+            </legend>
+            <fieldset>
+              <label for='email'>Email Address</label>
+              <input type='email' name='email' placeholder='name@address.com' />
+              <label for='password'>Password</label>
+              <input type='password' name='password' placeholder='Enter your password' />
+            </fieldset>
+            <button type='submit'>Sign In</button>
+          </form>
+          <small>Need an account? <a href='/users/signup'>Sign Up</a></small>
+        </div>
+        <div className='right'>
+          <img src={happiness} alt='happiness' />
+        </div>
       </div>
     )
   }
