1638c8d899df3117bb769e0a398cdd57e1664501
Starlight Romero <starlightromero@gmail.com>
2020-10-10 12:55:14 -0700

    styled SearchBar

--- src/components/Main/SearchBar.js
+++ src/components/Main/SearchBar.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
+import './SearchBar.css'
 
 class SearchBar extends Component {
     render() {
         return (
-            <div className='SearchBar m-t-10'>
-                <form>
+            <div className='SearchBar'>
+                <form className='m-t-10 m-l-10 m-r-10'>
                     <input type='text' placeholder={this.props.placeHolder} />
                 </form>
             </div>
