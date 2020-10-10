2912a317e15612a3017adc123b84a4d3ea2a253e
Starlight Romero <starlightromero@gmail.com>
2020-10-10 11:50:45 -0700

    changed margins to use classes

--- src/components/Main/UserGroup.js
+++ src/components/Main/UserGroup.js
@@ -3,14 +3,14 @@ import React, {Component} from 'react'
 class UserGroup extends Component {
     render() {
         return(
-            <div className='UserGroup'>
-                <h4 className="m-b-20">{this.props.groupTitle}</h4>
+            <div className='UserGroup m-t-15 m-b-15 m-l-15'>
+                <h4 className="m-b-30">{this.props.groupTitle}</h4>
                 { this.props.userItems.map((item) => {
                     return (
-                        <div className='UserItem m-b-10'>
+                        <div className='UserItem m-b-20'>
                             <div className='UserIcon' />
-                            <div className='User m-t-n20'>
-                                <div className='UserName'>
+                            <div className='User m-l-10'>
+                                <div className='UserName m-b-5'>
                                     <h4>{item.name}</h4>
                                 </div>
                                 <div className='UserStatus'>
