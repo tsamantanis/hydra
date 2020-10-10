362a326e920e3c777181a19a42f50a60734d2691
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-10-10 09:54:49 -0700

    Created user loader callback.

--- hydra/users/utils.py
+++ hydra/users/utils.py
@@ -1,6 +1,8 @@
 """Dependency and package import."""
 from flask import request, g, abort
+from hydra import db, jwt
 from jwt import decode, exceptions
+from flask_jwt_extended import user_loader_callback_loader
 from functools import wraps
 import json
 
@@ -35,3 +37,20 @@ def login_required(f):
         return f(*args, **kwargs)
 
     return wrap
+
+
+# Define user loader function: to be called every time a protected route is
+# accessed. Query may need to be modified
+
+
+@jwt.user_loader_callback_loader
+def userLoaderCallback(identity):
+    """
+    Query database for user id that matches passed id.
+
+    If user does not exist, return None. Else, return the user.
+    """
+    user = db.users.find_one_or_404({"_id": identity})
+    if identity not in user:
+        return None
+    return user
