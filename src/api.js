b0b9db118b54ecac650b0a47bfaa967a5caf8893
tsamantanis <philip.tsamantanis@icloud.com>
2020-10-10 12:38:41 -0700

    Fix camel case

--- src/api.js
+++ src/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_API_URL,
+    method:'post',
+    // url:"http://localhost:5000/send",
+    headers: {'content-type': 'application/json'},
+    // data: this.state
 });
 
 export default api;
