5a042c5e7aa48dd78ee876811ad15781be2896cd
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-10-09 23:53:45 -0700

    Fixed merge conflict, building User class.

---
+++ hydra/users/user.py
@@ -0,0 +1,11 @@
+"""Dependencies and package import."""
+
+class User:
+    """Create User class."""
+
+    def __init__(self, firstName, lastName, email, password):
+        """Initialize properties of users."""
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.password = password
