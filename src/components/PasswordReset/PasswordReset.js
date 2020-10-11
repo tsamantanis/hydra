c3fc29f137d6d723a51a7571344e3358c851bf65
Starlight Romero <starlightromero@gmail.com>
2020-10-11 12:55:37 -0700

    added post request to sendtoken

--- src/components/PasswordReset/PasswordReset.js
+++ src/components/PasswordReset/PasswordReset.js
@@ -6,6 +6,27 @@ import '../../App.css'
 import coworking from '../../assets/coworking.svg'
 
 class PasswordReset extends Component {
+    sendResetToken () {
+        const email = document.querySelector('input[name=email]').value
+        if (email) {
+            api({
+                method: 'POST',
+                url: '/users/passwordreset/sendtoken',
+                data: {
+                    "email": email
+                },
+            })
+            .then(function (response) {
+                console.log(response)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+        } else if (!email) {
+            console.log('Please enter an email.')
+        }
+    }
+
     render () {
         return (
             <div className='PasswordReset splitView'>
@@ -19,7 +40,7 @@ class PasswordReset extends Component {
                             <label htmlFor='email'>Email Address</label>
                             <input type='email' name='email' placeholder='name@address.com' />
                         </fieldset>
-                        <button type='submit'>Reset Password</button>
+                        <button type='submit' onClick={this.sendResetToken}>Reset Password</button>
                     </form>
                     <small>Remember your password? <a href='/users/signin'>Sign In</a></small>
                 </div>
