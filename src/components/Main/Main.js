eab9dd4662573f9d968fb04028f908965891dd55
tsamantanis <philip.tsamantanis@icloud.com>
2020-10-11 15:26:52 -0700

    Add class button

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -28,6 +28,8 @@ class Main extends Component {
                 <div className='Nav'>
                     <div className='NavItem m-b-20' />
                     <div className='NavItem m-b-20' />
+                    <div className="AddClass">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>                    </div>
                     <Settings />
                 </div>
                 <div className={this.state.showCommunity ? 'Channels' : 'Channels Channels-lg'}>
