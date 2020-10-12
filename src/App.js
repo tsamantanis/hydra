fcdd8dfacb70ac36e9461318811103926584554e
Starlight Romero <starlightromero@gmail.com>
2020-10-11 17:51:22 -0700

    finished Discover

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Main from './components/Main/Main'
 import SignUp from './components/SignUp/SignUp'
 import SignIn from './components/SignIn/SignIn'
 import PasswordReset from './components/PasswordReset/PasswordReset'
+import Discover from './components/Discover/Discover'
 
 class App extends Component {
     // constructor(props) {
@@ -28,6 +29,7 @@ class App extends Component {
           <Route exact path='/users/signup' component={SignUp} />
           <Route exact path='/users/signin' component={SignIn} />
           <Route exact path='/users/passwordreset' component={PasswordReset} />
+          <Route exact path='/discover' component={Discover} />
           <Route exact path='/' component={Main} />
           {/* <Route path='/' component={RedirectToLogin} /> */}
         </Switch>
---
+++ src/components/Discover/Discover.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+
+import api from '../../api'
+
+import '../../App.css'
+import './Discover.css'
+import ellipse from '../../assets/ellipse.svg'
+import school from '../../assets/school.svg'
+
+class Discover extends Component {
+    render () {
+        return (
+            <div className='Discover'>
+                <img src={ellipse} alt='ellipse' className='ellipse' />
+                <img src={school} alt='school' className='school' />
+                <form>
+                    <label htmlFor='discoverClasses'><h4 className='m-0'>Discover Classes</h4></label>
+                    <input type='text' name='discoverClasses' id='discoverClasses' placeholder='Search from a list of thousands of classes' required />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Discover
