9e042b4e5287e4e003676cfb93ad462286e21879
Starlight Romero <starlightromero@gmail.com>
2020-10-12 02:39:41 -0700

    removed unused method

--- src/components/Main/NavItem.js
+++ src/components/Main/NavItem.js
@@ -2,11 +2,6 @@ import React, {Component} from 'react'
 import './NavItem.css'
 
 class NavItem extends Component {
-
-    displayChannel(channelName) {
-        alert(channelName)
-    }
-
     render() {
         return (
             <div className='NavItem m-b-20'>
