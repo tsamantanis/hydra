079c8996632ab3d031b2eefa4478c4ab45303a49
Starlight Romero <starlightromero@gmail.com>
2020-10-12 02:10:06 -0700

    added state and method for assignments

--- src/components/Main/ChannelList.js
+++ src/components/Main/ChannelList.js
@@ -7,7 +7,8 @@ class ChannelList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            channels: []
+            channels: [],
+            assignments: []
         }
     }
 
@@ -32,6 +33,23 @@ class ChannelList extends Component {
         })
     }
 
+    getAssignments = () => {
+        const _this = this
+        this.setState({assignments: []})
+        api({
+            method: 'GET',
+            url: '/groups/5f83e890d1bf28e13820a756/assignments'
+        })
+        .then(function (response) {
+            for (const assignment of response.data) {
+                _this.setState({assignments: [..._this.state.assignments, assignment.name]})
+            }
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
+    }
+
 
 
     render() {
@@ -44,8 +62,8 @@ class ChannelList extends Component {
                 />
                 <ChannelGroup
                     channelLabel='Assignments'
-                    channelNames={this.state.channels}
-                    getChannels={this.getChannels}
+                    channelNames={this.state.assignments}
+                    getChannels={this.getAssignments}
                 />
             </>
         )
