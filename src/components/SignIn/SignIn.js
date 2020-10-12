590caad32427ce339f26a23849211eeed35cbfad
Starlight Romero <starlightromero@gmail.com>
2020-10-11 17:29:28 -0700

    added redirect to homepage upon successful login

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -21,6 +21,7 @@ class SignIn extends Component {
             })
             .then(function (response) {
                 console.log(response)
+                window.location.href='/'
             })
             .catch(function (error) {
                 console.log(error)
