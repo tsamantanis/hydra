19cb5fe0f10ae9cdf4e5abf8314584b23a495860
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-10-11 23:49:23 -0700

    Debugging, cleaning up code & error handling

--- BackendApi/hydra/channels/routes.py
+++ BackendApi/hydra/channels/routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, jsonify, request
 from hydra import db
 from flask_login import current_user, login_required
 from bson.json_util import dumps
+from bson.objectid import ObjectId
 
 channels = Blueprint("channels", __name__)
 
@@ -23,20 +24,29 @@ def showChannels(groupId):
 # @login_required
 def createChannel(groupId):
     """Create channel document in database."""
-
-    # TODO: Ensure that we're adding channelid to the group object
-    # TODO: Add category to channel object (Assignments, lectures, chat)
     name = request.json.get("name")
     dis = request.json.get("dis")
-    newChannel = {"name": name, "dis": dis}
-    insertedChannel = db.channels.insert_one(newChannel)
-    return jsonify({"msg": "{{name}} has been added"})
+    category = request.json.get("category")
+    if name and dis and category:
+        group = db.Group.find_one_or_404({"_id": ObjectId(groupId)})
+        newChannel = {
+            "name": name,
+            "dis": dis,
+            "category": category,
+            "groupId": groupId,
+        }
+        insertedChannel = db.channels.insert_one(newChannel)
+        group["channelsIds"].append(insertedChannel.inserted_id)
+        return jsonify({"msg": f"{name} has been added"})
+    else:
+        return jsonify({"msg": "Missing information, please try again."})
 
 
-@channels.route("/delete", methods=["POST"])
-@login_required
-def deleteChannel(groupId):
-    pass
+@channels.route("/delete/<channelId>", methods=["DELETE"])
+def deleteChannel(groupId, channelId):
+    channel = db.channels.find_one_or_404({"_id": ObjectId(channelId)})
+    db.channels.delete_one(channel)
+    return jsonify({"msg": "This channel has been deleted"})
 
 
 @channels.route("/<channelId>")
