3b89cbbb71480cc57b43c607d6da0f96d4102a63
tsamantanis <philip.tsamantanis@icloud.com>
2020-10-12 16:41:16 -0700

    Minor fixes

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -104,7 +104,7 @@ class Main extends Component {
                     <NavItem />
                     <NavItem />
                     <Link to="/discover" className="AddClass">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" className="feather feather-plus">
                             <line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line>
                         </svg>
                     </Link>
