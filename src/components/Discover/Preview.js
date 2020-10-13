bf2b0a9543ea035a34659c1726f25043b7a3c672
Starlight Romero <starlightromero@gmail.com>
2020-10-12 17:18:03 -0700

    removed console.log

--- src/components/Discover/Preview.js
+++ src/components/Discover/Preview.js
@@ -28,7 +28,6 @@ class Preview extends Component {
         })
         .then((response) => {
             this.setState({group: response.data})
-            console.log(this.state.group)
         })
         .catch((error) => {
             console.log(error)
